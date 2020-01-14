b30b1816ec6ad64e8ef439b3c601cc2989665b02
student <student@Admins-iMac.local>
2020-01-14 16:54:30 -0600

    EOD Tues - WIP

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("Hello World!");
+// name
+let name = "Kimberly Gawlowicz"
+
+// constant
+const states = "50";
+
+//  sum 2 numbers
+let sum = (5 + 4);
+
+console.log(name);
+console.log(states);
+console.log(sum);
+
+function sayHello() {
+    console.log("Hello World!");
+}
+
+sayHello();
+
+function checkAge(name, age) {
+    if (age < 21) {
+        console.log("Sorry " + name + ", you aren't old enough to view this page!");
+    } 
+}
+
+checkAge("Charles", 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John",17);
+
+var favVeg = ["tomato", "cucumber", "lettuce"];
+console.log(favVeg[0]);
+÷
+// let i=1;
+// while (i<4) {
+//     checkAge("Kim",18);
+//     i++;
+// }
+
+// let myObj = {Charles: 21, Abby: 27, James: 18, John: 17};
+// let n = myObj.length;
+// console.log("length of myObj is ", n)
+
